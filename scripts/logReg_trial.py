d36a589eb79245eede09cc0c44cf7958d9e96d7d
Lerac1275 <carelchay@outlook.com>
2022-10-08 11:03:56 +0800

    added print statements to track progress

--- scripts/logReg_trial.py
+++ scripts/logReg_trial.py
@@ -1,3 +1,4 @@
+print("Importing Packages . . .", end = "")
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
@@ -13,6 +14,7 @@ if pathname not in sys.path:
     sys.path.append(path_to_scripts)
 
 import getData as gd
+print("Done\n")
 
 # Path to pickle file with all the processed features
 path_to_pkl="/home/ubuntu/studies/ProjectStorage/data/parsed_data_carel.pkl"
@@ -22,20 +24,25 @@ path_to_report = ""
 ##### Data Preparation #####
 
 # Read in pkl file with the consolidated features
+print("Reading in pickle data . . .", end = "")
 df = pd.read_pickle(path_to_pkl)
 df = df[['transcript', 'position',
        'k-mer bases', 'f1_stats', 'f2_stats', 'f3_stats', 'f4_stats',
        'f5_stats', 'f6_stats', 'f7_stats', 'f8_stats', 'f9_stats']]
-
+print("Done\n")
 # Get the label dataframe
+print("Obtaining Label Data . . .", end="")
 getData = gd.getData()
 label_df = getData.get_labels()
+print("Done\n")
 
 # Merge the two sources
+print("Merging Data . . .", end="")
 df = pd.merge(df, label_df, how='left', left_on=("transcript", "position"),
              right_on=("transcript_id", "transcript_position"))
-
+print("Done\n")
 # Remove unnecessary columns
+print("Performing Column Transformations . . .", end="")
 df = df.filter(regex=".*_stats|label")
 
 # Each column right now is a column of lists of 6 elements each. Spread them out into separate unique features
@@ -46,19 +53,21 @@ for idx, col in enumerate(feature_cols):
                                                           axis=1)
 # Remove unnecessary columns
 df = df[df.columns.difference(feature_cols)]
-
+print("Done\n")
 ##### Obtain Training Data #####
 
 # data
+print("Obtaining Training Data . . .", end="")
 X = np.array(df[df.columns.difference(['label'])])
 y = np.array(df['label'])
 
 # Split Data
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
-
+print("Done\n")
 ##### Model Fitting & Evaluation #####
 
 # Logistic regression model with l2 norm regularization (ridge regression)
+print("Performing fitting & Evaluation . . .", end="")
 LogReg = LogisticRegression(solver="liblinear", penalty="l2")
 # Fit model
 LogReg.fit(X_train, y_train)
@@ -73,9 +82,11 @@ dummy_clf.fit(X_train, y_train)
 y_pred_dummy=dummy_clf.predict(X_test)
 # Store Confusion Matrix
 dummy_report = classification_report(y_test, y_pred_dummy)
+print("Done\n")
 
 # Write out the report
+print("Writing to report . . .", end="")
 with open(path_to_report, "w") as f:
     f.write(f"Logistic Ridge Regression summary report : \n\n{ridge_LogReg_report}\n\n")
     f.write(f"Dummy Classifier (Majority class) summary report : \n\n{dummy_report}")
-
+print("Done\n")
