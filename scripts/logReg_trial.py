640d6e88eadcf9ad65971d2b3d965cc561ec053b
Lerac1275 <carelchay@outlook.com>
2022-10-08 10:40:32 +0800

    script for logistic regression trial

---
+++ scripts/logReg_trial.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.dummy import DummyClassifier
+import re
+import sys
+
+# Add the scripts folder to the path so we can import the helper class later on
+path_to_scripts = ""
+if pathname not in sys.path:
+    sys.path.append(path_to_scripts)
+
+import getData as gd
+
+# Path to pickle file with all the processed features
+path_to_pkl="/home/ubuntu/studies/ProjectStorage/data/parsed_data_carel.pkl"
+# Path to the report output folder
+path_to_report = ""
+
+##### Data Preparation #####
+
+# Read in pkl file with the consolidated features
+df = pd.read_pickle(path_to_pkl)
+df = df[['transcript', 'position',
+       'k-mer bases', 'f1_stats', 'f2_stats', 'f3_stats', 'f4_stats',
+       'f5_stats', 'f6_stats', 'f7_stats', 'f8_stats', 'f9_stats']]
+
+# Get the label dataframe
+getData = gd.getData()
+label_df = getData.get_labels()
+
+# Merge the two sources
+df = pd.merge(df, label_df, how='left', left_on=("transcript", "position"),
+             right_on=("transcript_id", "transcript_position"))
+
+# Remove unnecessary columns
+df = df.filter(regex=".*_stats|label")
+
+# Each column right now is a column of lists of 6 elements each. Spread them out into separate unique features
+feature_cols = list(filter(lambda x : re.search("stats", x) is not None, df.columns))
+for idx, col in enumerate(feature_cols):
+    df[[f"{idx+1}_{x}" for x in range(1, 7)]] = df.apply(lambda x : x[col],
+                                                           result_type="expand",
+                                                          axis=1)
+# Remove unnecessary columns
+df = df[df.columns.difference(feature_cols)]
+
+##### Obtain Training Data #####
+
+# data
+X = np.array(df[df.columns.difference(['label'])])
+y = np.array(df['label'])
+
+# Split Data
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+##### Model Fitting & Evaluation #####
+
+# Logistic regression model with l2 norm regularization (ridge regression)
+LogReg = LogisticRegression(solver="liblinear", penalty="l2")
+# Fit model
+LogReg.fit(X_train, y_train)
+# Make Prediction
+y_pred = LogReg.predict(X_test)
+# Store Confusion matrix
+ridge_LogReg_report = classification_report(y_test, y_pred)
+
+# Predict everything as the majority class
+dummy_clf = DummyClassifier(strategy="most_frequent")
+dummy_clf.fit(X_train, y_train)
+y_pred_dummy=dummy_clf.predict(X_test)
+# Store Confusion Matrix
+dummy_report = classification_report(y_test, y_pred_dummy)
+
+# Write out the report
+with open(path_to_report, "w") as f:
+    f.write(f"Logistic Ridge Regression summary report : \n\n{ridge_LogReg_report}\n\n")
+    f.write(f"Dummy Classifier (Majority class) summary report : \n\n{dummy_report}")
+
