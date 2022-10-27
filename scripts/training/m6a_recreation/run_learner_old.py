e50e16356fef416add4e1b469db34111c7d416ce
Lerac1275 <carelchay@outlook.com>
2022-10-27 17:39:19 +0800

    updated run_learner.py to take command line arguments instead

--- scripts/training/m6a_recreation/run_learner_old.py
+++ scripts/training/m6a_recreation/run_learner_old.py
@@ -6,23 +6,6 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from run_validation import run_val
-import argparse
-
-parser = argparse.ArgumentParser(description='Run the training script for m6aNet')
-parser.add_argument("ptd", type=str, help="Path to datafile (json format)")
-parser.add_argument("ptl", type=str, help="Path to labels for the training data")
-parser.add_argument("ptp", type=str, help="Path to parameters folder. Where to place model parameter files after each epoch")
-parser.add_argument("ptvo", type=str, help="Where to output the validation dataset with attached probability scores (should be a csv file)")
-parser.add_argument("pteo", type=str, help="Where to output the dataframe of epoch vs avg loss (should be a csv file)")
-
-parser.add_argument("-nentries", type=int, help="How many entries from the training data to use. \nSet to 0 to use all data. \nFor the training dataset need to use at least 5000 entries ", default=5000)
-parser.add_argument("-batchsize", type=int, help="Batchsize for training", default=128)
-parser.add_argument("-lr", type=float, help="Learning Rate", default=0.01)
-parser.add_argument("-nepoch", type=int, help="Number of Epochs", default=50)
-
-args = parser.parse_args()
-
-
 
 def incremental_avg(old_n, old_avg, value):
     """
@@ -32,38 +15,38 @@ def incremental_avg(old_n, old_avg, value):
 
 
 # Where is the raw Data Stored
-# path_to_data = "/Users/carelchay/Desktopp/School/Modules/DSA4262/Project 2/data/data.json"
+path_to_data = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/data.json"
 # Where are the data labels stored
-# path_to_labels = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/data.info"
+path_to_labels = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/data.info"
 # Where to place model parameter files for each epoch
-# path_to_params = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/model_params"
+path_to_params = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/model_params"
 # Where to output the validation dataset with attached probability scores
-# path_to_val_output = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/val_output.csv"
+path_to_val_output = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/val_output.csv"
 # Where to output the dataframe of epoch vs avg loss
-# path_to_epoch_output = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/epoch_output.csv"
+path_to_epoch_output = "/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/data/epoch_output.csv"
 
 
 # How many entries from the raw data to use
-# num_entries = 5000
+num_entries = 5000
 # Batchsize for training
-# batchsize = 128
+batchsize = 128
 # Learning rate
-# learning_rate = 0.01
+learning_rate = 0.01
 # Number of Epochs
-# num_epochs = 5
+num_epochs = 5
 
-print(f"Using {args.nentries:,} entries with batchsize : {args.batchsize}\n")
+print(f"Using {num_entries:,} entries with batchsize : {batchsize}\n")
 
 # Add scripts folder to path
 import sys
-pathname="/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/DSA4262-frontasticfour/scripts/utils"
+pathname="/Users/carelchay/Desktop/School/Modules/DSA4262/Project 2/DSA4262-frontasticfour/scripts"
 if pathname not in sys.path:
     sys.path.append(pathname)
 
 ### Get the training data ###
 print("Importing & Obtaining label Dataframe . . . ", end= "")
-import getData as gd
-getData = gd.getData(path_to_data=args.ptd, path_to_labels=args.ptl)
+import getDataCarel as gd
+getData = gd.getData(path_to_data=path_to_data, path_to_labels=path_to_labels)
 # Get the dataframe with the labels
 label_df = getData.get_labels()
 print("Done\n")
@@ -78,32 +61,32 @@ train_genes = prepD.splitdata(label_df = label_df, train=0.85)
 
 # Get the data class for the TRAINING data
 trainData = prepD.prepData(train_genes=train_genes, train_set=True,
-                          path_to_data=args.ptd, path_to_labels=args.ptl,
-                          num_entries=args.nentries)
+                          path_to_data=path_to_data, path_to_labels=path_to_labels,
+                          num_entries=num_entries)
 
 # Get the dataloader object
-trainDataLoader = trainData.get_data_loader(batchsize=args.batchsize)
+trainDataLoader = trainData.get_data_loader(batchsize=batchsize)
 training_size = len(trainData)
 
-num_steps = math.ceil(training_size/args.batchsize)
+num_steps = math.ceil(training_size/batchsize)
 
 # Get the data class for the EVAL data
 evalData = prepD.prepData(train_genes=train_genes, train_set=False,
-                          path_to_data=args.ptd, path_to_labels=args.ptl,
-                          num_entries=args.nentries)
+                          path_to_data=path_to_data, path_to_labels=path_to_labels,
+                          num_entries=num_entries)
 
 # Get the dataloader object for the EVAL data
-evalDataLoader = evalData.get_data_loader(batchsize=args.batchsize, oversample=False, shuffle=False)
+evalDataLoader = evalData.get_data_loader(batchsize=batchsize, oversample=False, shuffle=False)
 
 
 
 print("Done")
-print(f"Total of {training_size} entries in training set, with batchsize : {args.batchsize}, total of {num_steps} steps per epoch. \n")
+print(f"Total of {training_size} entries in training set, with batchsize : {batchsize}, total of {num_steps} steps per epoch. \n")
 
 ### Creation of model ###
 print("Importing & creating instance of neural network . . . ", end= "")
 from m6aNet import m6aNet
-model = m6aNet(batchsize=args.batchsize)
+model = m6aNet(batchsize=batchsize)
 print("Done\n")
 
 ### Training Parameters ###
@@ -114,8 +97,8 @@ print("Done\n")
 criterion = nn.BCELoss()
 # Use Stochastic Gradient Descent. Need to supply the model parameters
 # and a selected learning rate
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0)
-print(f"Using {args.nepoch} epochs, learning rate = {args.lr}")
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0)
+print(f"Using {num_epochs} epochs, learning rate = {learning_rate}")
 
 ### Training Loop ###
 
@@ -138,7 +121,7 @@ triggertimes = 0
 
 
 
-for epoch in range(args.nepoch):
+for epoch in range(num_epochs):
     model.train()
     step=0
     # The average loss for this epoch
@@ -162,13 +145,13 @@ for epoch in range(args.nepoch):
 
 
         if (i + 1) % stepsize == 0:
-            print(f'Epoch [{epoch + 1}/{args.nepoch}], Step [{i + 1}/{num_steps}], Loss: {loss.item():.4f}, Avg Loss this epoch : {avg_epoch_loss:.4f}')
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{num_steps}], Loss: {loss.item():.4f}, Avg Loss this epoch : {avg_epoch_loss:.4f}')
 
     # Record the avg loss for this epoch
     epoch_loss.append(avg_epoch_loss)
 
     # Save the model state at this epoch
-    epoch_path = f"{args.ptp}/epoch_{epoch}.pth"
+    epoch_path = f"{path_to_params}/epoch_{epoch}.pth"
     torch.save(model.state_dict(), epoch_path)
 
     # Compute the validation loss for this epoch
@@ -199,15 +182,23 @@ tdf = pd.DataFrame({
     "avg_validation_loss" : validation_losses
 })
 
-tdf.to_csv(args.pteo, index=False)
+tdf.to_csv(path_to_epoch_output, index=False)
 
 #### Perform Evaluation ####
 
 # Instantiate model again
-model = m6aNet(batchsize=args.batchsize)
+model = m6aNet(batchsize=batchsize)
 # Loads the last stored model parameters
 model.load_state_dict(torch.load(epoch_path))
 model.eval()
+
+# # Get the data class for the EVAL data
+# data = prepD.prepData(train_genes=train_genes, train_set=False,
+#                           path_to_data=path_to_data, path_to_labels=path_to_labels,
+#                           num_entries=num_entries)
+#
+# # Get the dataloader object for the EVAL data
+# dataLoader = data.get_data_loader(batchsize=batchsize, oversample=False, shuffle=False)
 
 # Probability Threshold for M6A modification
 threshold = 0.8
@@ -247,4 +238,4 @@ val_output = evalData.df[['gene_id', 'transcript', 'position', 'label']].copy()
 val_output['pred_score'] = np.array(results_scores)
 
 # output to path
-val_output.to_csv(args.ptvo, index=False)
+val_output.to_csv(path_to_val_output, index=False)
